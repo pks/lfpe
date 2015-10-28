ec2db5d7367b90d752225193e834c158ce980c73
Patrick Simianer <p@simianer.de>
2015-10-28 10:03:29 +0100

    cosmetics

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 <!-- Footer -->
 <p id="footer">
+  <img id="uni" style="width:129px" src="img/logo_neu_204x107.jpg" />
+  <img id="cl" style="width:128px"  src="img/institut_cl.png" /><br />
   &copy;2015 Heidelberg University/Institute for Computational Linguistics
 </p>
 <!-- /Footer -->
--- header.php
+++ header.php
@@ -3,8 +3,6 @@
 
 <!-- Header -->
 <div id="header">
-  <img id="uni" style="width:129px" src="img/logo_neu_204x107.jpg" />
-  <img id="cl" style="width:128px"  src="img/institut_cl.png" />
 </div>
 <!-- /Header -->
 
