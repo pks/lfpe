c8fb5e8dbb8c8e54c8925ead0b83fcfdb933592e
Patrick Simianer <p@simianer.de>
2015-10-29 17:41:41 +0100

    help style

--- help.inc.php
+++ help.inc.php
@@ -3,7 +3,7 @@ Alternatively, in the textual interface, you may just press return when you fini
 <p>The session can be stopped at any time and continued later; However, if you have to pause your session, wait until the activity notification disappears and then press 'Pause', as we are collecting timing information. You may also just reload this site and re-request the next segment.</p>
 <p>Please only use a single browser window at once. Going back to earlier examples is not possible, please take great care when interacting with the system.</p>
 
-<p><strong>Instructions for graphical interface:</strong></p>
+<p><span style="border-bottom:1px solid #ccc">Instructions for graphical interface:</span></p>
 <ul>
   <li><strong>Reordering of target phrases:</strong> Drag and drop the phrase (box).</li>
   <li><strong>Adding/removing links:</strong> Select a source phrase by clicking on its box, then click on a target phrase (click selected source phrase again to cancel linking).</li>
