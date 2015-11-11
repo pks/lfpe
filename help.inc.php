e4c217da2949983b210ba8020a170d55b2b064cd
Patrick Simianer <p@simianer.de>
2015-11-11 16:11:20 +0100

    updated help, added known issues

--- help.inc.php
+++ help.inc.php
@@ -1,17 +1,26 @@
 <p style="margin-top:-1em">Press the 'Next' button to <strong>submit</strong> your post-edit and to request the next segment for post-edition.
 Alternatively, in the textual interface, you may just press return when you finished the post-edit ('Target' text area is in focus).</p>
 <p>The session can be stopped at any time and continued later; However, if you have to pause your session, wait until the activity notification disappears and then press 'Pause', as we are collecting timing information. You may also just reload this site and re-request the next segment.</p>
-<p>Please only use a single browser window at once. Going back to earlier examples is not possible, please take great care when interacting with the system.</p>
+<p>Please use only a <strong>single browser window</strong> at the same time. Going back to earlier examples is not possible, please take great care when interacting with the system.</p>
 
 <p><span style="border-bottom:1px solid #ccc">Instructions for graphical interface:</span></p>
 <ul>
-  <li><strong>Reordering of target phrases:</strong> Drag and drop the phrase (box).</li>
-  <li><strong>Adding/removing links:</strong> Select a source phrase by clicking on its box, then click on a target phrase (click selected source phrase again to cancel linking).</li>
-  <li><strong>Edit text:</strong> Double click on text, click outside once to stop editing (or press return).</li>
+  <li><strong>Reordering of target phrases:</strong> Drag and drop the boxes.</li>
+  <li><strong>Adding/removing links:</strong> Select a source phrase by clicking on its box, then click on a target phrase (click selected source phrase again to cancel).</li>
+  <li><strong>Edit text:</strong> Double click on text, click outside once or press return to stop editing.</li>
   <li><strong>Adding target phrases:</strong> Click '+' button to add a new phrase at the end of the translation. Ctrl-click an existing phrase to add a new phrase to the right of it.</li>
-  <li><strong>Removing target phrases:</strong> Drag phrase onto left-hand red box. You can also shift-click on a phrase and press 'delete'. Selecting multiple phrases using shift-click is also possible.</li>
+  <li><strong>Removing target phrases:</strong> Shift-click on a phrase and press the 'delete' key. Selecting multiple phrases using shift-click is also possible.</li>
   <li><strong>Reset:</strong> Click 'Reset' button to start from scratch.</li>
 </ul>
 
 <p>The interface was tested with Firefox 31 and 38.</p>
+<p><strong>Known issues:</strong></p>
+<ul>
+  <li>collision detection is not perfect, fast dragging may lead to strange behavior</li>
+  <li>graphical editor can still be used while faded</li>
+  <li>width of the canvas of graphical editor may be to small when adding a lot of phrases</li>
+  <li>in-line editor background is red</li>
+  <li>in-line editor may change height</li>
+  <li>when editing phrases with no content input is lower</li>
+</ul>
 
