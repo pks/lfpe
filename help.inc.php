2d844881e3c1e25728038fe531ff7e2e87b8f569
Patrick Simianer <p@simianer.de>
2015-11-23 11:00:24 +0100

    help

--- help.inc.php
+++ help.inc.php
@@ -1,26 +1,28 @@
 <p style="margin-top:-1em">Press the 'Next' button to <strong>submit</strong> your post-edit and to request the next segment for post-edition.
 Alternatively, in the textual interface, you may just press return when you finished the post-edit ('Target' text area is in focus).</p>
-<p>The session can be stopped at any time and continued later; However, if you have to pause your session, wait until the activity notification disappears and then press 'Pause', as we are collecting timing information. You may also just reload this site and re-request the next segment.</p>
+<p>The session can be paused at any time and continued later; However, if you have to pause your session, wait until the activity notification disappears and then press 'Pause', as we are collecting timing information. You may also just reload this site and re-request the segment to reset the timer.</p>
 <p>Please use only a <strong>single browser window</strong> at the same time. Going back to earlier examples is not possible, please take great care when interacting with the system.</p>
 
-<p><span style="border-bottom:1px solid #ccc">Instructions for graphical interface:</span></p>
+<p><span style="border-bottom:1px solid #ccc">Instructions for the graphical interface:</span></p>
 <ul>
-  <li><strong>Reordering of target phrases:</strong> Drag and drop the boxes.</li>
-  <li><strong>Adding/removing links:</strong> Select a source phrase by clicking on its box, then click on a target phrase (click selected source phrase again to cancel).</li>
-  <li><strong>Edit text:</strong> Double click on text, click outside once or press return to stop editing.</li>
-  <li><strong>Adding target phrases:</strong> Click '+' button to add a new phrase at the end of the translation. Ctrl-click an existing phrase to add a new phrase to the right of it.</li>
-  <li><strong>Removing target phrases:</strong> Shift-click on a phrase and press the 'delete' key. Selecting multiple phrases using shift-click is also possible.</li>
+  <li><strong>Moving around:</strong> Press <strong>'S'</strong>, then move around using the arrow keys.</li>
+  <li><strong>Reordering of target phrases:</strong> Press <strong>'M'</strong>, then use the arrow keys.</li>
+  <li><strong>Adding/removing alignments:</strong> Select a source phrase by left-clicking on its box, then click on a suitable target phrase (click selected source phrase again to cancel).</li>
+  <li><strong>Edit text:</strong> Double click on text, click outside once or press return to stop editing. Alternatively, press <strong>'E'</strong> to edit the contents of the current phrase.</li>
+  <li><strong>Adding target phrases:</strong> Click '+' button to add a new phrase at the end of the translation. To add a phrase right next to the currently selected phrase just press <strong>'A'</strong>.</li>
+  <li><strong>Removing target phrases:</strong> Alt-click a phrase and press <strong>'D'</strong> key. Selecting multiple phrases for deletion is also possible. To delete the currently selected phrase just press <strong>'D'</strong>.</li>
   <li><strong>Reset:</strong> Click 'Reset' button to start from scratch.</li>
 </ul>
 
 <p>The interface was tested with Firefox 31 and 38.</p>
-<p><strong>Known issues:</strong></p>
+
+<p><span style="border-bottom:1px solid #ccc">Known issues:</span></p>
 <ul>
-  <li>collision detection is not perfect, fast dragging may lead to strange behavior</li>
-  <li>graphical editor can still be used while faded</li>
-  <li>width of the canvas of graphical editor may be to small when adding a lot of phrases</li>
-  <li>in-line editor background is red</li>
-  <li>in-line editor may change height</li>
-  <li>when editing phrases with no content input is lower</li>
+<!--  <li>collision detection is not perfect, fast dragging may lead to strange behavior</li>-->
+<!--  <li>graphical editor can still be used while faded</li>-->
+  <li>The width of the canvas of graphical editor may be to small when adding a lot of phrases</li>
+<!--  <li>in-line editor background is red</li>-->
+  <li>The in-line editor may change its height</li>
+  <li>When editing phrases with no content input box is lower than normal</li>
 </ul>
 
