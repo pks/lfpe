913d602044ef8083ba5dcf68b46b075cf345006f
Patrick Simianer <p@simianer.de>
2016-06-07 17:09:47 +0200

    index.php: use pool

--- index.php
+++ index.php
@@ -18,18 +18,90 @@
     <option value="g">graphical</option>
     <option value="t">text</option>
 </select>
-&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;
 <input type="submit" value="Submit" />
 </form>
 
-<div class="small" style="background:#eee;margin: 5em 0 5em 0;padding:.5em; max-width:25%">
+<!--<div class="small" style="background:#eee;margin: 5em 0 5em 0;padding:.5em; max-width:25%">
 <p>Select:
 <select class="small">
 
-  <option value="final_model_en-de_g0_demo_nomt" onclick="document.getElementById('key').value=this.value;">Demo (no PE, text only)</option>
-  <option value="final_model_en-de_g0_demo_pe" onclick="document.getElementById('key').value=this.value;">Demo (PE)</option>
+  <option value="g0_0_nomt" onclick="document.getElementById('key').value=this.value;">#0 (from scratch)</option>
+  <option value="g0_0_pe" onclick="document.getElementById('key').value=this.value;">#0 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_1_nomt" onclick="document.getElementById('key').value=this.value;">#1 (from scratch)</option>
+  <option value="g0_1_pe" onclick="document.getElementById('key').value=this.value;">#1 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_2_nomt" onclick="document.getElementById('key').value=this.value;">#2 (from scratch)</option>
+  <option value="g0_2_pe" onclick="document.getElementById('key').value=this.value;">#2 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_3_nomt" onclick="document.getElementById('key').value=this.value;">#3 (from scratch)</option>
+  <option value="g0_3_pe" onclick="document.getElementById('key').value=this.value;">#3 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_4_nomt" onclick="document.getElementById('key').value=this.value;">#4 (from scratch)</option>
+  <option value="g0_4_pe" onclick="document.getElementById('key').value=this.value;">#4 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_5_nomt" onclick="document.getElementById('key').value=this.value;">#5 (from scratch)</option>
+  <option value="g0_5_pe" onclick="document.getElementById('key').value=this.value;">#5 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_6_nomt" onclick="document.getElementById('key').value=this.value;">#6 (from scratch)</option>
+  <option value="g0_6_pe" onclick="document.getElementById('key').value=this.value;">#6 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_7_nomt" onclick="document.getElementById('key').value=this.value;">#7 (from scratch)</option>
+  <option value="g0_7_pe" onclick="document.getElementById('key').value=this.value;">#7 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_8_nomt" onclick="document.getElementById('key').value=this.value;">#8 (from scratch)</option>
+  <option value="g0_8_pe" onclick="document.getElementById('key').value=this.value;">#8 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_9_nomt" onclick="document.getElementById('key').value=this.value;">#9 (from scratch)</option>
+  <option value="g0_9_pe" onclick="document.getElementById('key').value=this.value;">#9 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_10_nomt" onclick="document.getElementById('key').value=this.value;">#10 (from scratch)</option>
+  <option value="g0_10_pe" onclick="document.getElementById('key').value=this.value;">#10 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_11_nomt" onclick="document.getElementById('key').value=this.value;">#11 (from scratch)</option>
+  <option value="g0_11_pe" onclick="document.getElementById('key').value=this.value;">#11 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_12_nomt" onclick="document.getElementById('key').value=this.value;">#12 (from scratch)</option>
+  <option value="g0_12_pe" onclick="document.getElementById('key').value=this.value;">#12 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_13_nomt" onclick="document.getElementById('key').value=this.value;">#13 (from scratch)</option>
+  <option value="g0_13_pe" onclick="document.getElementById('key').value=this.value;">#13 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_14_nomt" onclick="document.getElementById('key').value=this.value;">#14 (from scratch)</option>
+  <option value="g0_14_pe" onclick="document.getElementById('key').value=this.value;">#14 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_15_nomt" onclick="document.getElementById('key').value=this.value;">#15 (from scratch)</option>
+  <option value="g0_15_pe" onclick="document.getElementById('key').value=this.value;">#15 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_16_nomt" onclick="document.getElementById('key').value=this.value;">#16 (from scratch)</option>
+  <option value="g0_16_pe" onclick="document.getElementById('key').value=this.value;">#16 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_17_nomt" onclick="document.getElementById('key').value=this.value;">#17 (from scratch)</option>
+  <option value="g0_17_pe" onclick="document.getElementById('key').value=this.value;">#17 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_18_nomt" onclick="document.getElementById('key').value=this.value;">#18 (from scratch)</option>
+  <option value="g0_18_pe" onclick="document.getElementById('key').value=this.value;">#18 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_19_nomt" onclick="document.getElementById('key').value=this.value;">#19 (from scratch)</option>
+  <option value="g0_19_pe" onclick="document.getElementById('key').value=this.value;">#19 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_20_nomt" onclick="document.getElementById('key').value=this.value;">#20 (from scratch)</option>
+  <option value="g0_20_pe" onclick="document.getElementById('key').value=this.value;">#20 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_21_nomt" onclick="document.getElementById('key').value=this.value;">#21 (from scratch)</option>
+  <option value="g0_21_pe" onclick="document.getElementById('key').value=this.value;">#21 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_22_nomt" onclick="document.getElementById('key').value=this.value;">#22 (from scratch)</option>
+  <option value="g0_22_pe" onclick="document.getElementById('key').value=this.value;">#22 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_23_nomt" onclick="document.getElementById('key').value=this.value;">#23 (from scratch)</option>
+  <option value="g0_23_pe" onclick="document.getElementById('key').value=this.value;">#23 (post-editing)</option>
+<optgroup label="________________"></optgroup>
+  <option value="g0_24_nomt" onclick="document.getElementById('key').value=this.value;">#24 (from scratch)</option>
+  <option value="g0_24_pe" onclick="document.getElementById('key').value=this.value;">#24 (post-editing)</option>-->
 
-<!--<optgroup label="________________">
+<!--<optgroup label="________________________">
   <option value="product_de-en_toy_example" onclick="document.getElementById('key').value=this.value;">toy example</option>
 </optgroup>-->
 
@@ -61,9 +133,11 @@
   <option value="product_en-de_beta_test_1_D" onclick="document.getElementById('key').value=this.value;">D* en-de</option>
 </optgroup>-->
 
-</select>
+<!--</select>
 </p>
-</div>
+</div>-->
+
+<a style="font-size:1.2em;color:#000" href="pool.php">Assignment</a>
 
 <?php include("inc/footer.inc.php"); ?>
 
